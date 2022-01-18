215a775db8cdc7f327d8a779dfb5717051a99caa
Muhammad Arham Saqib <arhamsaqib@outlook.com>
2022-01-18 19:21:11 +0500

    added: constants

---
+++ src/constants/colors.ts
@@ -0,0 +1,11 @@
+export const COLORS = {
+  MAIN_1: '#61963A',
+  MAIN_2: '#70B140',
+  MAIN_TEXT: '#222222',
+  MAIN_SUBTEXT: '#444444',
+  MAIN_BODYTEXT: '#666666',
+  WHITE: '#FFFFFF',
+  light_green: '#eaf1e6',
+  WF_TITLE: 'black',
+  WF2: '#f8f8f8',
+};
---
+++ src/constants/fonts.ts
@@ -0,0 +1,7 @@
+export const FONTS = {
+  P_REGULAR: 'Poppins-Regular',
+  P_BOLD: 'Poppins-Bold',
+  P_MEDIUM: 'Poppins-Medium',
+  P_SEMIBOLD: 'Poppins-SemiBold',
+  P_Light: 'Poppins-Light',
+};
