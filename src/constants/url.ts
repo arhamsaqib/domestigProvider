e0e63ce27ffabf2ff1b2805cd8be20172719728f
Muhammad Arham Saqib <arhamsaqib@outlook.com>
2022-03-13 20:06:00 +0500

    added google maps api key

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,2 +1,4 @@
 export const URL = 'http://127.0.0.1:8000/api/'; //IOS
 //export const URL = "http://10.0.2.2:8000/api/"; //Android
+
+// export const GOOGLE_MAPS_API_KEY = 'AIzaSyDlMojnSF1RqIUXwYb0IyHIZXXGodvLPQQ';
